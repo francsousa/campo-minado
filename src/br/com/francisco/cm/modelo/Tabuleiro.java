80e137087fd553bd5e51793ddf652f40fccce7b7
Francisco Sousa <franciscoeds1987@hotmail.com>
2020-07-27 10:28:52 -0300

    Adicionado lógica de mostrar o tabuleiro após o jogador perder

--- src/br/com/francisco/cm/modelo/Tabuleiro.java
+++ src/br/com/francisco/cm/modelo/Tabuleiro.java
@@ -1,5 +1,7 @@
 package br.com.francisco.cm.modelo;
 
+import br.com.francisco.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -23,10 +25,15 @@ public class Tabuleiro {
     }
 
     public void abrir(int linha, int coluna) {
-        campos.parallelStream()
-                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
-                .findFirst()
-                .ifPresent(Campo::abrir);
+        try {
+            campos.parallelStream()
+                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                    .findFirst()
+                    .ifPresent(Campo::abrir);
+        } catch (ExplosaoException e) {
+            campos.forEach(c -> c.setAberto(true));
+            throw e;
+        }
     }
 
     public void alternarMarcacao(int linha, int coluna) {
